92fbb842c3d5963c6dfa9ea1ce4848e1c2bc2a89
Mukunda Janardhana <mukunda.mj@gmail.com>
2018-12-20 10:04:30 +0100

    Setup script modified

--- setupMyMachine.sh
+++ setupMyMachine.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
-if [ -z "$1" ] 
-  then
-    echo "IP address in the argument is missing"
-    exit
-fi
-ssh root@$1 "mkdir -p ~/.vim/colors"
-scp ~/sandbox/mjanardh/machineSetup/vimSetupFiles/solarized.vim root@$1:~/.vim/colors/solarized.vim
-scp ~/sandbox/mjanardh/machineSetup/vimSetupFiles/.vimrc root@$1:~/.vimrc
-scp ~/sandbox/mjanardh/machineSetup/runTestXtimes.sh root@$1:/ghostcache/runTestXtimes.sh
-cat ~/sandbox/mjanardh/machineSetup/bashSetupFiles/myaliases | ssh root@$1 "cat >> ~/.bashrc" #To append my aliases to the ~/.bashrc file on the remote machine
-ssh root@$1 "source ~/.bashrc"
+
+echo "Machine setup started"
+
+touch ~/.vimrc
+cat vim/.vimrc | >> ~/.vimrc
+
+touch ~/.bashrc
+cat .bashrc | >> ~/.bashrc
+
+touch ~/.bash_profile
+cat .bash_profile | >> ~/.bash_profile
+
+touch ~/.tmux.conf
+cat tmux/.tmux.conf | >> ~/.tmux.conf
+
 echo "Machine setup completed"
